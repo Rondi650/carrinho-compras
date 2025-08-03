b57b9c42a8cc51793a31cbd280f1b1341680964d
Rondi650 <rondinellenunes123@gmail.com>
2025-08-02 21:09:07 -0300

    versao atualizada

--- js/app.js
+++ js/app.js
@@ -1,33 +1,32 @@
+let totalGeral = 0
+limpar()
 
-let resultadoGlobal = 0
 
 function adicionar() {
-    let quantidade = parseInt(document.getElementById('quantidade').value)
-    let produtoSelecionado = document.getElementById('produto').value;
-    let apenasValorProduto = parseInt(produtoSelecionado.replace(/\D+/g, ''))
-    let apenasTextoProduto = produtoSelecionado.replace(/[-–]? ?R\$?[\d,.]+/gi, "")
-    console.log(apenasTextoProduto)
+    //recuperar valores, nome do produto, quantidade e valor
+    let produto = document.getElementById('produto').value
+    let nomeProduto = produto.split('-')[0]
+    let valorUnitario = parseInt(produto.split('R$')[1])
+    let quantidade = document.getElementById('quantidade').value
 
-    resultadoGlobal = resultadoGlobal + (quantidade * apenasValorProduto)
-    console.log(resultadoGlobal)
+    //calcular preco, o nosso subtotal
+    let preco = valorUnitario * quantidade
 
-    //document.querySelector('.carrinho__produtos').innerHTML = ''
-
-    document.querySelector(".carrinho__produtos").insertAdjacentHTML(
-        'beforeend',
-        `<section class="carrinho__produtos__produto">
-        <span class="texto-azul">${quantidade}x</span> ${apenasTextoProduto} <span class="texto-azul">R$${quantidade * apenasValorProduto}</span>
+    //adicionar no carrinho
+    let carrinho = document.getElementById('lista-produtos')
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
     </section>`
-    );
 
-    document.querySelector(".carrinho__total").innerHTML = 
-    `Total: <span class="texto-azul" id="valor-total">R$${resultadoGlobal}</span>`
-
+    //atualizar o valor total
+    totalGeral = totalGeral + preco
+    let campoTotal = document.getElementById('valor-total')
+    campoTotal.textContent = `R$ ${totalGeral}`
+    document.getElementById('quantidade').value = 0
 }
 
 function limpar() {
-    resultadoGlobal = 0
-    document.querySelector(".carrinho__produtos").innerHTML = ''
-    document.querySelector(".carrinho__total").innerHTML = 
-    `Total: <span class="texto-azul" id="valor-total">R$0</span>`
+    totalGeral = 0
+    document.getElementById('lista-produtos').innerHTML = ''
+    document.getElementById('valor-total').innerHTML = 'R$ 0'
 }
